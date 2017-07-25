4079d679d0e6d448a33519e18c2822deadceb1b0
jochenberger <fooberger@gmail.com>
2017-07-25 13:55:42 +0200

    fix typo

--- src/main/java/javax/ws/rs/core/Application.java
+++ src/main/java/javax/ws/rs/core/Application.java
@@ -53,7 +53,7 @@ import java.util.Set;
  * {@link javax.ws.rs.core.Context}.
  * </p>
  * <p>
- * In case any of the {@code Application} subclass methods or it's constructor
+ * In case any of the {@code Application} subclass methods or its constructor
  * throws a {@link RuntimeException}, the deployment of the application SHOULD
  * be aborted with a failure.
  * </p>
